8d6d2e5083f6a21c62bf4fbac2c6e3d6fe1fed6e
wuxieveronica24-coder <wuxieveronica24@gmail.com>
2026-02-22 23:20:47 +0000

    commit see how it goes

--- game/main.java
+++ game/main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner; 
+
 public class Main{
     public static void main(String[] args){
-        String user_name = "Jane Doe";
+        String user_name;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the name you wish to have: ")
+        user_name = scanner.nextLine;
         System.out.println(user_name);
 
     }
