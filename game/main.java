bab2cd948fab8148d365e2b5aa9859f9a64a142b
wuxieveronica24-coder <wuxieveronica24@gmail.com>
2026-02-22 23:21:23 +0000

    just commit

--- game/main.java
+++ game/main.java
@@ -5,7 +5,7 @@ public class Main{
         String user_name;
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter the name you wish to have: ")
+        System.out.println("Enter the name you wish to have: ");
         user_name = scanner.nextLine;
         System.out.println(user_name);
 
