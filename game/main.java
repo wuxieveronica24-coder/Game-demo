8e8020ac722aeb30b3f56db180f4ba7e0e0b2dc6
wuxieveronica24-coder <wuxieveronica24@gmail.com>
2026-02-22 23:10:46 +0000

    commit dk what's wrong

--- game/main.java
+++ game/main.java
@@ -1,7 +1,7 @@
-Public class Main{
-    Public static void main(String[] args){
+public class Main{
+    public static void main(String[] args){
         String user_name = "Jane Doe";
         System.out.println(user_name);
-        
+
     }
 }
