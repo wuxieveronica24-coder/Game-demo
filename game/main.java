43da0d7d2967926e456a8b0b2bedbc505d457ec7
Veronica Wu Xie <wuxieveronica24@gmail.com>
2026-02-23 22:50:00 +0000

    t

--- game/main.java
+++ game/main.java
@@ -19,7 +19,7 @@ public class Main{
 
 // complete with help from stack overflow 
 
-public static void slow_print(String whatever, TimeUnit unit, long time_take) throws InterruptedException {
+public String slow_print(String whatever, TimeUnit unit, long time_take) throws InterruptedException {
     for (char c: whatever.toCharArray()){
         System.out.println(c);
         unit.sleep(time_take);
