1edcca77d8fa9464ee013ed805e8662f07d958b2
Veronica Wu Xie <wuxieveronica24@gmail.com>
2026-02-23 22:27:17 +0000

    b

--- game/main.java
+++ game/main.java
@@ -9,17 +9,6 @@ public class Main{
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("loading .........");
-        
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1); // 1 is the size of the thread pool being created
-
-        System.out.println("Scheduling a task with a 3-second delay");
-
-        scheduler.schedule(() -> {
-            System.out.println("Task executed after 3 seconds"); // Task to be executed after the delay
-        }, 3, TimeUnit.SECONDS); // Adding the 3-second delay
-
-        scheduler.shutdown();
-
 
         System.out.println("Enter the name you wish to have: ");
         user_name = scanner.nextLine();
